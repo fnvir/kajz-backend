b5acd1b2d12b483fe94bdda96c046d6ad351bf48
Farhan Tanvir <farhantaanvir@gmail.com>
2026-01-13 05:29:25 +0600

    Remove unused NotNull import from OtpVerifcationReq

--- kajz/auth-service/src/main/java/dev/fnvir/kajz/authservice/dto/req/OtpVerifcationReq.java
+++ kajz/auth-service/src/main/java/dev/fnvir/kajz/authservice/dto/req/OtpVerifcationReq.java
@@ -3,7 +3,6 @@ package dev.fnvir.kajz.authservice.dto.req;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record OtpVerifcationReq(
         @NotBlank(message = "User ID cannot be blank.")
